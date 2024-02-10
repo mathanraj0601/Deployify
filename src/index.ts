3f92de1e9e8988bf200b77571e647653c521be36
Mathanraj <madhanmani0601@gmail.com>
2024-02-10 23:48:08 +0530

    Added url endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(cors());
 
+app.use("/deploy", (req, res) => {
+  const { repoUrl } = req.body;
+  res.json({
+    data: true,
+    error: null,
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.info(`Server is listening on port :${port}`);
